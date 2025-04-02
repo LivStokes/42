e20a1e01dbabca85aae2cb7cbdb054a6d28bd71b
LivStokes <Oliviastokess02@gmail.com>
2025-04-02 18:17:26 +0200

    Add files via upload

---
+++ c00/ex05/ft_print_comb.c
@@ -0,0 +1,55 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_comb.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: olstokes <olstokes@student.42berlin.d      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/30 14:32:36 by olstokes          #+#    #+#             */
+/*   Updated: 2025/04/02 13:44:18 by olstokes         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include <unistd.h>
+
+void	ft_putchar(char a, char b, char c)
+{
+	write(1, &a, 1);
+	write(1, &b, 1);
+	write(1, &c, 1);
+	if (!(a == '7' && b == '8' && c == '9'))
+	{
+		write(1, ", ", 2);
+	}
+}
+
+void	ft_print_comb(void)
+{
+	int	a;
+	int	b;
+	int	c;
+
+	a = '0';
+	while (a <= '7')
+	{
+		b = a + 1;
+		while (b <= '8')
+		{
+			c = b + 1;
+			while (c <= '9')
+			{
+				ft_putchar(a, b, c);
+				c++;
+			}
+			b++;
+		}
+		a++;
+	}
+}
+/*
+int	main(void)
+{
+	ft_print_comb();
+	write(1, "\n", 1);
+	return (0);
+}
+*/
