974dd63d0197ec5b3bafe6b11225344b05945d16
LivStokes <Oliviastokess02@gmail.com>
2025-04-06 17:53:29 +0200

    Add files via upload

---
+++ rush01/c02/ex00/ft_strcpy.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strcpy.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: olstokes <olstokes@student.42berlin.d      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/03 20:15:43 by olstokes          #+#    #+#             */
+/*   Updated: 2025/04/06 10:32:20 by olstokes         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+//#include <stdio.h>
+
+char	*ft_strcpy(char *dest, char *src)
+{
+	int	i;
+
+	i = 0;
+	while (src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
+/*
+int	main()
+{
+	char src[] = "Hello World!";
+	char dest[50];
+
+	ft_strcpy(dest, src);
+	printf("Copied String: %s\n", dest);
+
+	return 0;
+}
+*/
