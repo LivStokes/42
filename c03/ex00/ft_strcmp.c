c86354f9c8a4c187991ab78e13989d478e61d3ab
LivStokes <Oliviastokess02@gmail.com>
2025-04-07 18:47:21 +0200

    Add files via upload

--- c03/ex00/ft_strcmp.c
+++ c03/ex00/ft_strcmp.c
@@ -1,4 +1,17 @@
-int	ft_strcmp(char *s1, char *s2);
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strcmp.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: olstokes <olstokes@student.42berlin.d      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/07 16:47:29 by olstokes          #+#    #+#             */
+/*   Updated: 2025/04/07 16:47:32 by olstokes         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+//#include <unistd.h>
+int	ft_strcmp(char *s1, char *s2)
 {
 	int	i;
 
@@ -9,3 +22,19 @@ int	ft_strcmp(char *s1, char *s2);
 	}
 	return (s1[i] - s2[i]);
 }
+
+/*
+int	main(int argc, char *argv[])
+{
+	if (argc != 3)
+	{
+		write(1, "more than 2 strings\n", 20);
+		return (1);
+	}
+	if (ft_strcmp(argv[1], argv[2]) == 0)
+		write(1, "same\n", 5);
+	else
+		write(1, "not same\n", 9);
+	return (0);
+}
+*/
--- c03/ex01/ft_strncmp.c
+++ c03/ex01/ft_strncmp.c
@@ -3,10 +3,16 @@ int	ft_strncmp(char *s1, char *s2, unsigned int n)
 	unsigned int	i;
 
 	i = 0;
-	while (i < n && s1[i] && s2[i] && s1[i] == s2[i])
+	while (i < n && s2[i] != '\0')
+	{
+		s1[i] = s2[i];
 		i++;
-	if (i == n)
-		return 0;
-	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+	}
+	while (i < n)
+	{
+		s1 = '\0'
+		i++;
+	}
+	return (s1);
 }
 
